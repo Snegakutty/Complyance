48dee60d5ef7b08c7f17ac416e11cba1d49e5f97
Snegakutty <132530737+Snegakutty@users.noreply.github.com>
2025-10-07 17:20:33 +0530

    be

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
+
+import axios from "axios";
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // update when deployed
+  baseURL: "https://complyance-ir2u.onrender.com",
 });
 export default API;
